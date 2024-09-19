7d2df6f220640a5e1c30b6c0a853f59636e4e551
Yusuf-Kilinc <104257668+Yusuf-Kilinc@users.noreply.github.com>
2024-09-19 21:11:02 +0300

    Data
    Data

---
+++ Assets/PlayerTaken.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerTaken : MonoBehaviour
+{
+    public Transform Player;
+
+    Vector3 pos,pos1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = new Vector3(Player.transform.position.x, Player.transform.position.y, -10);
+        pos1 = new Vector3(transform.position.x, transform.position.y, -10);
+
+
+       // this.transform.position = pos;
+
+        this.transform.position = Vector3.Lerp(pos1, pos, 1 * Time.deltaTime);
+    }
+}
