76a5c46f64fad94433567b57d0505412e0d5b053
Yusuf-Kilinc <104257668+Yusuf-Kilinc@users.noreply.github.com>
2024-09-29 17:16:51 +0300

    Data
    Data

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -3,10 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "ScaneFinish")
         {
+            Debug.Log("Degdi");
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
